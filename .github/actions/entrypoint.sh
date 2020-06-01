d62a4ed728dc70ec8c6c090cf96a4c0f0620a13c
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-01 22:06:49 +0200

    Fix typo in entrypoint.sh

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -10,4 +10,4 @@ chmod 700 "${SSH_PATH}"
 echo "${DEPLOY_KEY_PRIVATE}" > "${SSH_PATH}/${KEY_FILENAME}"
 chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
 
-echo "${SSH_PATH}/${KEY_FILENAME}"
+cat "${SSH_PATH}/${KEY_FILENAME}"
