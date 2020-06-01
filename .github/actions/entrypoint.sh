1994487144e98f26edccfbb47ee9d138f7a986e8
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-01 22:13:38 +0200

    Test action rebuild site

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -10,4 +10,15 @@ chmod 700 "${SSH_PATH}"
 echo "${DEPLOY_KEY_PRIVATE}" > "${SSH_PATH}/${KEY_FILENAME}"
 chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
 
-cat "${SSH_PATH}/${KEY_FILENAME}"
+echo "Setting git config globally"
+git config --global user.email "${EMAIL}"
+git config --global user.name "${GITHUB_ACTOR}"
+
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+cd "${GITHUB_WORKSPACE}"/public
+git checkout master
+git add .
+git commit -m "rebuild site at $(date)"
+git push origin master
