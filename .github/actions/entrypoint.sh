27a0a8c5658b6cf3d8cace90974f98aebbe1b53c
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-01 22:19:32 +0200

    Test github rebuild

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -11,8 +11,10 @@ echo "${DEPLOY_KEY_PRIVATE}" > "${SSH_PATH}/${KEY_FILENAME}"
 chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
 
 echo "Setting git config globally"
+echo "${GITHUB_ACTOR}"
 git config --global user.email "${EMAIL}"
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "Mingyi Zhang"
+git config --global core.sshCommand 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /root/.ssh/id_rsa -F /dev/null'
 
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
