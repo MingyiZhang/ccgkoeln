4381076b1933c170f75605aa72348c2f50542f66
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-01 22:01:03 +0200

    Test action

---
+++ .github/actions/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+SSH_PATH="/root/.ssh"
+KEY_FILENAME="id_rsa"
+mkdir -p "${SSH_PATH}"
+chmod 700 "${SSH_PATH}"
+
+echo "${DEPLOY_KEY_PRIVATE}" > "${SSH_PATH}/${KEY_FILENAME}"
+chmod 600 "${SSH_PATH}/${KEY_FILENAME}"
+
+echo "${SSH_PATH}/${KEY_FILENAME}"
